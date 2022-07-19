7e9148e27bece8506ca78be2a894d92f7376fa16
Rawan_Ashraf <rbahnasy98@gmail.com>
2022-07-19 15:53:57 +0200

    first commit

---
+++ app/src/main/java/com/example/marketplace/model/Response.kt
@@ -0,0 +1,36 @@
+package com.example.marketplace.model
+
+import java.util.*
+
+data class Response(
+    val country : String = "",
+    val itemName : String = "",
+    val price : Double = 0.0,
+    val currency : String = "",
+    val thumbnail : String = "",
+    val images : List<String> = listOf(),
+    val description : String = "",
+    val latitude : Double = 0.0,
+    val longitude : Double = 0.0,
+    val seller : Owner,
+    val horseInfo: HorseInfo? = null,
+    val isAvailable : Boolean = false,
+    val category : String,
+    val expiredDate : Date,
+)
+
+data class Owner(
+    val name : String = "",
+    val phoneNumber : Long = 0,
+    val email : String = ""
+)
+
+data class HorseInfo(
+    val hName : String ="",
+    val hColor : String = "",
+    val hSexType : String = "",
+    val hGender : String = "",
+    val hDOB : Date ,
+    val hBreed : String = "",
+    val hStrain : String = ""
+)
