499a4a86559d54e3d725ff686dd0ca444e05d15d
Rawan_Ashraf <rbahnasy98@gmail.com>
2022-07-20 11:13:52 +0200

    design spinner

--- app/src/main/java/com/example/marketplace/MainActivity.kt
+++ app/src/main/java/com/example/marketplace/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.marketplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.marketplace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setUpSpinner()
+    }
+    fun setUpSpinner(){
+        binding.apply {
+
+            ArrayAdapter.createFromResource(this@MainActivity,R.array.Countries,R.layout.spinner_text_view).also {
+                arrayAdapter -> arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+                spinner.adapter = arrayAdapter
+            }
+
+        }
     }
 }
